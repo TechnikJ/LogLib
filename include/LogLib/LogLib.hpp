67563ebcded76d0e92627f74649c4a4e836a4d3d
TechnikJ <jasonrobo27@gmail.com>
2021-10-13 15:32:29 +0200

    LogLib.hpp

--- include/LogLib/LogLib.hpp
+++ include/LogLib/LogLib.hpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <ctime>
 #include <filesystem>
+#include <source_location>
 
 class Log {
 
@@ -42,7 +43,7 @@ class Log {
             this->FileStream.open(this->filePath);
         }
 
-        void  writeInfo(std::string ctx){
+        void  writeInfo(std::string msg){
             
             time_t curr_time;
             tm * curr_tm;
@@ -55,9 +56,26 @@ class Log {
 
             std::string timestampStr(timestamp);
 
-            std::string logContent = "["+timestampStr+"] [INFO]"+": "+ctx;
+            std::string logContent = "["+timestampStr+"] [INFO]"+": "+msg;
 
             this->FileStream << logContent << std::endl;
+            this->FileStream.flush();
+        }
+
+        void writeError(std::string msg, std::string errorCode, const std::source_location location = std::source_location::current()){
+
+            time_t curr_time;
+            tm * curr_tm;
+
+            time(&curr_time);
+            curr_tm = localtime(&curr_time);
+
+            char* timestamp;
+            std::strftime(timestamp,50,"%H:%M:%S",curr_tm);
+
+            std::string timestampStr(timestamp);
+
+            this->FileStream << "[" << timestampStr+"] [ERROR] " << location.file_name() << "(" << location.line() << ":" << location.column() << ") `" << location.function_name() << "` : " << msg << std::endl;
             this->FileStream.flush();
         }
 };
