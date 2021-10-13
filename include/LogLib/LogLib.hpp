d9b2ac66d49fbeafca2b444ba09960049ad1624f
TechnikJ <jasonrobo27@gmail.com>
2021-10-13 17:13:55 +0200

    LogLib.hpp

--- include/LogLib/LogLib.hpp
+++ include/LogLib/LogLib.hpp
@@ -36,67 +36,68 @@ namespace ll{
                 time(&m_curr_time);
                 m_curr_tm = localtime(&m_curr_time);
 
-                char* timestamp;
-                std::strftime(timestamp,50,"%H-%M-%S_%d-%m-%Y",m_curr_tm);
+                char* m_timestamp;
+                std::strftime(m_timestamp,50,"%H-%M-%S_%d-%m-%Y",m_curr_tm);
 
-                std::string m_timestampStr(timestamp);
+                std::string m_timestampStr(m_timestamp);
                 std::string m_fileName = m_timestampStr+".log";
 
                 this->m_filePath = t_logDirectory+"/"+m_fileName;
                 this->m_fileStream.open(this->m_filePath);
             }
 
-            void writeInfo(std::string msg){
-                
-                time_t curr_time;
-                tm * curr_tm;
+            void write(std::string t_msg, SeverityLevel t_lev, const std::source_location t_location = std::source_location::current()){
+                time_t m_curr_time;
+                tm * m_curr_tm;
 
-                time(&curr_time);
-                curr_tm = localtime(&curr_time);
+                time(&m_curr_time);
+                m_curr_tm = localtime(&m_curr_time);
 
-                char* timestamp;
-                std::strftime(timestamp,50,"%H:%M:%S",curr_tm);
+                char* m_timestamp;
+                std::strftime(m_timestamp,50,"%H:%M:%S",m_curr_tm);
 
-                std::string timestampStr(timestamp);
+                std::string timestampStr(m_timestamp);
 
-                std::string logContent = "["+timestampStr+"] [INFO]"+": "+msg;
-
-                this->m_fileStream << logContent << std::endl;
+                switch(t_lev){
+                    case ll::SeverityLevel::INFO:
+                        this->m_fileStream << "[" << timestampStr << "] [INFO] : " << t_msg << std::endl;
+                    break;
+                    case ll::SeverityLevel::WARNING:
+                        this->m_fileStream << "[" << timestampStr << "] [WARN] " << t_location.file_name() << "(" << t_location.line() << ":" << t_location.column() << ") `" << t_location.function_name() << "` : " << t_msg << std::endl;
+                    break;
+                    case ll::SeverityLevel::ERROR:
+                        this->m_fileStream << "[" << timestampStr << "] [ERROR] " << t_location.file_name() << "(" << t_location.line() << ":" << t_location.column() << ") `" << t_location.function_name() << "` : " << t_msg << std::endl;
+                    break;
+                }
                 this->m_fileStream.flush();
             }
 
-            void writeError(std::string msg, std::string errorCode, const std::source_location location = std::source_location::current()){
+            void write(std::string t_msg, SeverityLevel t_lev, std::string t_code, const std::source_location t_location = std::source_location::current()){
+                time_t m_curr_time;
+                tm * m_curr_tm;
 
-                time_t curr_time;
-                tm * curr_tm;
+                time(&m_curr_time);
+                m_curr_tm = localtime(&m_curr_time);
 
-                time(&curr_time);
-                curr_tm = localtime(&curr_time);
+                char* m_timestamp;
+                std::strftime(m_timestamp,50,"%H:%M:%S",m_curr_tm);
 
-                char* timestamp;
-                std::strftime(timestamp,50,"%H:%M:%S",curr_tm);
+                std::string timestampStr(m_timestamp);
 
-                std::string timestampStr(timestamp);
-
-                this->m_fileStream << "[" << timestampStr+"] [ERROR] [code: "<< errorCode << "] " << location.file_name() << "(" << location.line() << ":" << location.column() << ") `" << location.function_name() << "` : " << msg << std::endl;
+                switch(t_lev){
+                    case ll::SeverityLevel::INFO:
+                        this->m_fileStream << "[" << timestampStr << "] [INFO] : " << t_msg << std::endl;
+                    break;
+                    case ll::SeverityLevel::WARNING:
+                        this->m_fileStream << "[" << timestampStr << "] [WARN] [code: "<< t_code << "] " << t_location.file_name() << "(" << t_location.line() << ":" << t_location.column() << ") `" << t_location.function_name() << "` : " << t_msg << std::endl;
+                    break;
+                    case ll::SeverityLevel::ERROR:
+                        this->m_fileStream << "[" << timestampStr << "] [ERROR] [code: "<< t_code << "] " << t_location.file_name() << "(" << t_location.line() << ":" << t_location.column() << ") `" << t_location.function_name() << "` : " << t_msg << std::endl;
+                    break;
+                }
                 this->m_fileStream.flush();
             }
 
-            void writeWarning(std::string msg, std::string warningCode, const std::source_location location = std::source_location::current()){
-
-                time_t curr_time;
-                tm * curr_tm;
-
-                time(&curr_time);
-                curr_tm = localtime(&curr_time);
-
-                char* timestamp;
-                std::strftime(timestamp,50,"%H:%M:%S",curr_tm);
-
-                std::string timestampStr(timestamp);
-
-                this->m_fileStream << "[" << timestampStr+"] [WARN] [code: "<< warningCode << "] " << location.file_name() << "(" << location.line() << ":" << location.column() << ") `" << location.function_name() << "` : " << msg << std::endl;
-                this->m_fileStream.flush();
-            }
+            
     };
 }
